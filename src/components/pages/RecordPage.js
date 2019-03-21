864f15ca98db72dc568597f20bfed8fb4098f531
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-03-21 10:26:16 +0900

    add: calculate latest event

--- src/components/pages/RecordPage.js
+++ src/components/pages/RecordPage.js
@@ -6,8 +6,8 @@ class RecordPage extends React.Component {
     super(props)
     this.state = {
       bunchName: "20卒バンチ",
-      bunchRecord: {
-        date: "2019-03"
+      displayEvent: {
+        date: ""
       },
       events: []
     }
@@ -20,10 +20,11 @@ class RecordPage extends React.Component {
   fetchResponse(){
     $.ajax({
         url: 'http://localhost:3001/test1',
-        ype: "GET"
+        type: "GET"
     })
     .done((data) => {
       this.setState({
+        displayEvent: data.events[data.events.length - 1],
         events: data.events
       });
     })
@@ -39,13 +40,13 @@ class RecordPage extends React.Component {
           <button id="record-header-button" data-toggle="collapse" data-target="#record-list">
             <div id="bunch-name">{this.state.bunchName}</div>
             <div id="bunch-record">
-              <div id="bunch-record-name">{this.state.bunchRecord.date} バンチ対抗戦 <span id="pulldown-mark"><i class="fas fa-angle-down"></i></span></div>
+              <div id="bunch-record-name">{this.state.displayEvent.date} バンチ対抗戦 <span id="pulldown-mark"><i class="fas fa-angle-down"></i></span></div>
               <div id="pulldown-mark-line"><i class="fas fa-angle-down"></i></div>
             </div>
           </button>
           <div id="record-list" className="collapse">
             {this.state.events.map(v => {
-              return <button onClick={this.setBunch.bind(this, v)} key={v["date"]}className="record-list-item" data-toggle="collapse" data-target="#record-list">{v["date"]} バンチ対抗戦</button>
+              return <button onClick={this.setEvent.bind(this, v)} key={v["date"]}className="record-list-item" data-toggle="collapse" data-target="#record-list">{v["date"]} バンチ対抗戦</button>
             })}
 
             <button id="list-close-button" data-toggle="collapse" data-target="#record-list">
@@ -60,9 +61,9 @@ class RecordPage extends React.Component {
     )
   }
 
-  setBunch(bunch){
-    console.log(bunch)
-    this.setState({bunchRecord: bunch})
+  setEvent(event){
+    console.log(event)
+    this.setState({displayEvent: event})
   }
 }
 
